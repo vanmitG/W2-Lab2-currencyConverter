c26bfb2a6f0c2701e16242cbc89c624704dc2b3d
vanmitG <mit.van@gmail.com>
2019-07-23 16:16:38 +0700

    add conversion api

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@
 // console.log("amountToConvertamountToConvert", amountToConvert);
 document.getElementById("amount").focus();
 
-const usdExchangeRate = 23216;
-const eurExchangeRate = 26130.8;
+// const usdExchangeRate = 23216;
+// const eurExchangeRate = 26130.8;
 
 //Format number to desired currency
 function currencyFormater(type, value) {
+  console.log("Currency Formatter Tupe: ", type);
+  console.log("value", value);
+
   const formater = new Intl.NumberFormat(type, {
     currency: type,
     style: "currency"
@@ -19,12 +22,12 @@ function currencyFormater(type, value) {
 /***************************************************** */
 /*Convert vnd to usd and reverse START */
 /************************************ */
-function vndToUsd(amountToConvert) {
-  return amountToConvert / usdExchangeRate;
-}
-function usdToVnd(amountToConvert) {
-  return amountToConvert * usdExchangeRate;
-}
+// function vndToUsd(amountToConvert) {
+//   return amountToConvert / usdExchangeRate;
+// }
+// function usdToVnd(amountToConvert) {
+//   return amountToConvert * usdExchangeRate;
+// }
 
 /************************************* */
 /*Convert vnd to usd and reverse END */
@@ -33,12 +36,12 @@ function usdToVnd(amountToConvert) {
 /***************************************************** */
 /*Convert vnd to eur and reverse START */
 /************************************* */
-function vndToEur(amountToConvert) {
-  return amountToConvert / eurExchangeRate;
-}
-function eurToVnd(amountToConvert) {
-  return amountToConvert * eurExchangeRate;
-}
+// function vndToEur(amountToConvert) {
+//   return amountToConvert / eurExchangeRate;
+// }
+// function eurToVnd(amountToConvert) {
+//   return amountToConvert * eurExchangeRate;
+// }
 
 // console.log("amountResult", amountResult);
 
@@ -55,33 +58,48 @@ function convertCurrency() {
   const toCurrency = document.getElementById("convertTo").value;
   // console.log("toCurrencytoCurrency", toCurrency);
 
-  let amountResult = 0;
-  if (fromCurrency === "vnd" && toCurrency === "usd") {
-    amountResult = vndToUsd(amountToConvert);
-  }
-
-  if (fromCurrency === "usd" && toCurrency === "vnd") {
-    amountResult = usdToVnd(amountToConvert);
-  }
-  if (fromCurrency === "vnd" && toCurrency === "eur") {
-    amountResult = vndToEur(amountToConvert);
-  }
-
-  if (fromCurrency === "eur" && toCurrency === "vnd") {
-    amountResult = eurToVnd(amountToConvert);
-  }
+  const amountResult = getConversion(fromCurrency, toCurrency);
+  // console.log(amountResult);
 
   //printed result to screen
-  document.getElementById("convertedResult").innerHTML = `${currencyFormater(
-    fromCurrency,
-    amountToConvert
-  )}  is equal to ${currencyFormater(toCurrency, amountResult)}`;
-  console.log("clicked");
+  // document.getElementById("convertedResult").innerHTML = `${currencyFormater(
+  //   fromCurrency,
+  //   amountToConvert
+  // )}  is equal to ${currencyFormater(toCurrency, amountResult)}`;
+  // console.log("clicked");
 }
 
-// function getConversion(from, to){
-//   var xhr = new XMLHttpRequest();
-//   xhr.open(
-//     "GET",
-//   )
-// }
+function updateResults(from, to, conversionRate) {
+  const value = document.getElementById("amount").value;
+  console.log("value redf 13", value);
+  console.log("value redf 13 conversionRate", conversionRate);
+  const convertedValue = conversionRate * value;
+  console.log("superman", convertedValue);
+  const formattedCurrency = currencyFormater(to.toUpperCase(), convertedValue);
+  document.getElementById("convertedResult").innerHTML = `${currencyFormater(
+    from,
+    value
+  )}  is equal to ${formattedCurrency}`;
+  // document.getElementById("convertedResult").innerHTML = formattedCurrency;
+  // return formattedCurrency;
+}
+
+function getConversion(from, to) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://free.currconv.com/api/v7/convert?q=${from}_${to}&compact=ultra&apiKey=9a02420c4a5e378f2215`
+  );
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      console.log("sjksjsjs", response);
+      const conversionRate =
+        response[`${from.toUpperCase()}_${to.toUpperCase()}`];
+      updateResults(from, to, conversionRate);
+    } else {
+      alert("Request failed.  Returned status of " + xhr.status);
+    }
+  };
+  xhr.send();
+}
